7611b8d9f8a3e9638973282bb86e2ae86d81829f
doreen <doreen20080615@gmail.com>
2026-02-01 22:44:42 +0800

    更新：環境設定、登入頁面

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import axios from 'axios';
+import './assets/style.css';
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container login">
+      <form className="form-floating">
+        <h1>請先登入</h1>
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            name="username"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="username">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        <button type="submit" className="btn btn-primary w-100 mt-2">
+          登入
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
